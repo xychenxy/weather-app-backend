189dce6210ba6cf4ba2dd1ef3d13092a7beb0efb
xychenxy <mavischen916@gmail.com>
2020-08-03 23:39:43 +1000

    update

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
 mongoose
-    .connect(process.env.DATABASE_URL, {useNewUrlParser:true,useUnifiedTopology:true})
+    .connect('mongodb://localhost:27017/weatherData', {useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=> console.log('DB connected'))
     .catch(err => console.log(`DB connection error: ${err}`))
 
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use('/weather', require('./routes/weather'))
 
